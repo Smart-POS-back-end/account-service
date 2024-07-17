886ae9c73090e94b474c50e4e4f632a521328e70
baga gusdiana <bagaman83@gmail.com>
2024-07-17 15:41:59 +0700

    EPOS 77 using role to limit access to endpoint (#9)
    EPOS-77 using ROLE to limit access to endpoint

--- src/main/java/com/pesona/group/account/application/api/AccountController.java
+++ src/main/java/com/pesona/group/account/application/api/AccountController.java
@@ -4,7 +4,9 @@ import com.pesona.group.account.service.UserService;
 import com.pesona.group.account.application.request.CreateUserAccountDto;
 import com.pesona.group.dto.Response.GlobalDatabaseActionResult;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,9 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/account")
 @RequiredArgsConstructor
+@Slf4j
 public class AccountController {
     private final UserService userService;
 
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
     @PostMapping("/register")
     public ResponseEntity<GlobalDatabaseActionResult> register(@RequestBody CreateUserAccountDto createUserAccountDto) {
         GlobalDatabaseActionResult globalDatabaseActionResult = userService.registerUser(createUserAccountDto);
